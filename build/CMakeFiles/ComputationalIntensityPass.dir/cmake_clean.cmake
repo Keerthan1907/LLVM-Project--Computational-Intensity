353e4d7293fcf636258652f1d2e56930d999c200
Keerthan1907 <keerthank1920@gmail.com>
2025-06-07 23:58:11 +0530

    add project

---
+++ build/CMakeFiles/ComputationalIntensityPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ComputationalIntensityPass.dir/ComputationalIntensityPass.cpp.o"
+  "CMakeFiles/ComputationalIntensityPass.dir/ComputationalIntensityPass.cpp.o.d"
+  "ComputationalIntensityPass.pdb"
+  "ComputationalIntensityPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ComputationalIntensityPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ComputationalIntensityPass.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ComputationalIntensityPass.
+# This may be replaced when dependencies are built.
